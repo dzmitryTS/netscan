df14ca858c6c4218449f637ba70a7bdc677b744a
dzmitryTS <36435353+dzmitryTS@users.noreply.github.com>
2019-03-26 15:21:19 +0300

    json dump fixed

--- arpReader.py
+++ arpReader.py
@@ -2,12 +2,12 @@ from python_arptable import get_arp_table
 import json
 
 def resolveIPbyMAC(mac):
-    hosts = json.loads(get_arp_table())
+    hosts = json.dumps(get_arp_table())
     for host in hosts:
         print(host)
 
 def testARP():
-    hosts = json.loads(get_arp_table())
+    hosts = json.dumps(get_arp_table())
     for host in hosts:
         print(host)
 
